caca550773dc919af3ab971c59d95435e14675be
alfisai <alfonsoisaia@gmail.com>
2016-05-27 08:12:07 -0600

    Update Sis16a.java

--- src/sis16a/Sis16a.java
+++ src/sis16a/Sis16a.java
@@ -17,6 +17,7 @@ public class Sis16a {
      */
     public static void main(String[] args) {
                 System.out.println("Hola mundo");
+                System.out.println("Holis luna");
     }
     
 }
